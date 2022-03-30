0913ebb74a8bbe20d4670df50035445b1dc068d4
Ashby McFall <ashbymcfall@hotmail.co.uk>
2022-03-30 21:57:04 +0900

    💄 add row dividers to table

--- components/table/table.tsx
+++ components/table/table.tsx
@@ -84,7 +84,7 @@ const Table = () => {
                 </a>
               </td>
             </tr>
-            <tr className="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
               <td className="px-6 py-4">Pull1</td>
               <td className="px-6 py-4">C1</td>
               <th
@@ -106,7 +106,7 @@ const Table = () => {
                 </a>
               </td>
             </tr>
-            <tr className="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
               <td className="px-6 py-4">Pull1</td>
               <td className="px-6 py-4">D1</td>
               <th
@@ -128,7 +128,7 @@ const Table = () => {
                 </a>
               </td>
             </tr>
-            <tr className="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
               <td className="px-6 py-4">Pull1</td>
               <td className="px-6 py-4">D2</td>
               <th
