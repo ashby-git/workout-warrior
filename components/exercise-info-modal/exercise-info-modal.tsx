998b40ade63574f650458b1dd83847baf54d9c8d
Ashby McFall <ashbymcfall@hotmail.co.uk>
2022-04-19 12:40:24 +0900

    💄add input form design to exercise-info-modal

--- components/exercise-info-modal/exercise-info-modal.tsx
+++ components/exercise-info-modal/exercise-info-modal.tsx
@@ -1,9 +1,9 @@
 import { Fragment, useRef, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
-import { ExclamationIcon } from "@heroicons/react/outline";
+import { TrashIcon, XIcon } from "@heroicons/react/outline";
 
 export default function ExerciseInfoModal() {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
 
   const cancelButtonRef = useRef(null);
 
@@ -15,7 +15,7 @@ export default function ExerciseInfoModal() {
         initialFocus={cancelButtonRef}
         onClose={setOpen}
       >
-        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div className="text-left flex justify-center items-center h-full">
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -27,66 +27,138 @@ export default function ExerciseInfoModal() {
           >
             <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
           </Transition.Child>
-
-          {/* This element is to trick the browser into centering the modal contents. */}
-          <span
-            className="hidden sm:inline-block sm:align-middle sm:h-screen"
-            aria-hidden="true"
-          >
-            &#8203;
-          </span>
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
-            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            enterTo="opacity-100 translate-y-0 sm:scale-100"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
             leave="ease-in duration-200"
-            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
           >
-            <div className="relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                <div className="sm:flex sm:items-start">
-                  <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                    <ExclamationIcon
-                      className="h-6 w-6 text-red-600"
-                      aria-hidden="true"
-                    />
-                  </div>
-                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                    <Dialog.Title
-                      as="h3"
-                      className="text-lg leading-6 font-medium text-gray-900"
-                    >
-                      Deactivate account
-                    </Dialog.Title>
-                    <div className="mt-2">
-                      <p className="text-sm text-gray-500">
-                        Are you sure you want to deactivate your account? All of
-                        your data will be permanently removed. This action
-                        cannot be undone.
-                      </p>
+            <div className="md:col-span-2 transform transition-all h-max w-full max-w-4xl">
+              <div className="flex justify-end translate-y-full">
+                <XIcon
+                  className="h-5 cursor-pointer"
+                  onClick={() => setOpen(false)}
+                />
+              </div>
+              <form action="#" method="POST">
+                <div className="shadow overflow-hidden sm:rounded-md">
+                  <div className="px-4 py-5 bg-white sm:p-6">
+                    <div className="grid grid-cols-6 gap-2 sm:gap-5">
+                      <div className="col-span-6 sm:col-span-3">
+                        <label className="block text-sm font-medium text-gray-700">
+                          Day
+                        </label>
+                        <input
+                          type="text"
+                          placeholder="e.g. Pull1 or Chest or Date"
+                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full 
+                  shadow-sm sm:text-sm border-gray-300 rounded-md"
+                        />
+                      </div>
+                      <div className="col-span-6 sm:col-span-3">
+                        <label className="block text-sm font-medium text-gray-700">
+                          Letter
+                        </label>
+                        <input
+                          type="text"
+                          placeholder="e.g. A1"
+                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full 
+                  shadow-sm sm:text-sm border-gray-300 rounded-md"
+                        />
+                      </div>
+                      <div className="col-span-6 sm:col-span-3">
+                        <label className="block text-sm font-medium text-gray-700">
+                          Exercise
+                        </label>
+                        <input
+                          type="text"
+                          placeholder="e.g. Pull Ups"
+                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full 
+                  shadow-sm sm:text-sm border-gray-300 rounded-md"
+                        />
+                      </div>
+                      <div className="col-span-6 sm:col-span-3">
+                        <label className="block text-sm font-medium text-gray-700">
+                          Intensity
+                        </label>
+                        <input
+                          type="text"
+                          placeholder="e.g. 15kg"
+                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full 
+                  shadow-sm sm:text-sm border-gray-300 rounded-md"
+                        />
+                      </div>
+                      <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                        <label className="block text-sm font-medium text-gray-700">
+                          Sets
+                        </label>
+                        <input
+                          type="text"
+                          placeholder="e.g. 3"
+                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full 
+                  shadow-sm sm:text-sm border-gray-300 rounded-md"
+                        />
+                      </div>
+                      <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label className="block text-sm font-medium text-gray-700">
+                          Reps
+                        </label>
+                        <input
+                          type="text"
+                          placeholder="e.g. 15"
+                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full 
+                  shadow-sm sm:text-sm border-gray-300 rounded-md"
+                        />
+                      </div>
+                      <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                        <label className="block text-sm font-medium text-gray-700">
+                          Rest
+                        </label>
+                        <input
+                          type="text"
+                          placeholder="e.g. 2min"
+                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full 
+                  shadow-sm sm:text-sm border-gray-300 rounded-md"
+                        />
+                      </div>
                     </div>
                   </div>
+                  <div className="px-4 py-3 bg-gray-50 text-right sm:px-6 sm:flex sm:flex-row sm:justify-end">
+                    <button
+                      type="submit"
+                      className="w-full inline-flex justify-center rounded-md border border-transparent 
+              shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white 
+              hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 
+              focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    >
+                      Save
+                    </button>
+                    <button
+                      type="button"
+                      className="sm:mt-0 mt-2 w-6/12 inline-flex justify-center rounded-md border border-gray-300 
+              shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 
+              hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 
+              focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm"
+                      onClick={() => setOpen(false)}
+                      // ref={cancelButtonRef}
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      type="button"
+                      className="w-6/12 inline-flex justify-center rounded-md border border-gray-300 
+              shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 
+              hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 
+              focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm align-bottom"
+                    >
+                      <TrashIcon className="h-6 sm:h-5" />
+                    </button>
+                  </div>
                 </div>
-              </div>
-              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                <button
-                  type="button"
-                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-                  onClick={() => setOpen(false)}
-                >
-                  Deactivate
-                </button>
-                <button
-                  type="button"
-                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                  onClick={() => setOpen(false)}
-                  ref={cancelButtonRef}
-                >
-                  Cancel
-                </button>
-              </div>
+              </form>
             </div>
           </Transition.Child>
         </div>
