0ffde328ca5c6638fbd12d01764943f852b29bcd
Ashby McFall <ashbymcfall@hotmail.co.uk>
2022-04-19 12:38:03 +0900

    💄 rearrange buttons on input form

--- components/exercise-info-modal/test-form.tsx
+++ components/exercise-info-modal/test-form.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { TrashIcon } from "@heroicons/react/outline";
-import { XIcon } from "@heroicons/react/outline";
+import { TrashIcon, XIcon } from "@heroicons/react/outline";
 
 const TestForm = () => {
   return (
@@ -91,19 +90,19 @@ const TestForm = () => {
               </div>
             </div>
           </div>
-          <div className="px-4 py-3 bg-gray-50 text-right sm:px-6 sm:flex sm:flex-row-reverse">
+          <div className="px-4 py-3 bg-gray-50 text-right sm:px-6 sm:flex sm:flex-row sm:justify-end">
             <button
-              type="button"
-              className="w-6/12 inline-flex justify-center rounded-md border border-gray-300 
-              shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 
-              hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 
-              focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm align-bottom"
+              type="submit"
+              className="w-full inline-flex justify-center rounded-md border border-transparent 
+              shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white 
+              hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 
+              focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
             >
-              <TrashIcon className="h-6 sm:h-5" />
+              Save
             </button>
             <button
               type="button"
-              className="w-6/12 inline-flex justify-center rounded-md border border-gray-300 
+              className="sm:mt-0 mt-2 w-6/12 inline-flex justify-center rounded-md border border-gray-300 
               shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 
               hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 
               focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm"
@@ -113,13 +112,13 @@ const TestForm = () => {
               Cancel
             </button>
             <button
-              type="submit"
-              className="mt-3 w-full inline-flex justify-center rounded-md border border-transparent 
-              shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white 
-              hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 
-              focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+              type="button"
+              className="w-6/12 inline-flex justify-center rounded-md border border-gray-300 
+              shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 
+              hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 
+              focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm align-bottom"
             >
-              Save
+              <TrashIcon className="h-6 sm:h-5" />
             </button>
           </div>
         </div>
