e0d8db44f7c2caf5734dcebb758879893d65be0b
Ashby McFall <ashbymcfall@hotmail.co.uk>
2022-05-02 20:40:38 +0900

    🚧 start adding timer functionality

--- components/timer/timer.tsx
+++ components/timer/timer.tsx
@@ -1,16 +1,118 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Timer = () => {
+  const [timeInSeconds, setTimeInSeconds] = useState(0);
+  const [timeArray, setTimeArray] = useState<Array<number | string>>([]);
+  const [intervalId, setIntervalId] = useState<number>(0);
+  const [disableStart, setDisableStart] = useState(false);
+  const [disableStop, setDisableStop] = useState(false);
+
+  useEffect(() => {
+    setTimeArray(calculateTimeInSeconds(timeInSeconds));
+  }, [timeInSeconds]);
+
+  const bgChange = disableStart
+    ? "bg-gradient-to-b from-gray-800 to-indigo-900"
+    : "bg-gradient-to-b from-gray-800 to-gray-700";
+
+  const addOneSecond = () => {
+    setTimeInSeconds((previousState: number) => previousState + 1);
+  };
+  const minusOneSecond = () => {
+    if (timeInSeconds > 0) {
+      setTimeInSeconds((previousState: number) => previousState - 1);
+    }
+  };
+  const addTenSeconds = () => {
+    setTimeInSeconds((previousState: number) => previousState + 10);
+  };
+  const minusTenSeconds = () => {
+    if (timeInSeconds >= 10) {
+      setTimeInSeconds((previousState: number) => previousState - 10);
+    }
+  };
+  const addOneMinute = () => {
+    setTimeInSeconds((previousState: number) => previousState + 60);
+  };
+  const addTenMinutes = () => {
+    setTimeInSeconds((previousState: number) => previousState + 600);
+  };
+  const minusOneMinute = () => {
+    if (timeInSeconds >= 60) {
+      setTimeInSeconds((previousState: number) => previousState - 60);
+    }
+  };
+  const minusTenMinutes = () => {
+    if (timeInSeconds >= 600) {
+      setTimeInSeconds((previousState: number) => previousState - 600);
+    }
+  };
+  const addOneHour = () => {
+    setTimeInSeconds((previousState: number) => previousState + 3600);
+  };
+  const addTenHours = () => {
+    setTimeInSeconds((previousState: number) => previousState + 36000);
+  };
+  const minusOneHour = () => {
+    if (timeInSeconds >= 3600) {
+      setTimeInSeconds((previousState: number) => previousState - 3600);
+    }
+  };
+  const minusTenHours = () => {
+    if (timeInSeconds >= 36000) {
+      setTimeInSeconds((previousState: number) => previousState - 36000);
+    }
+  };
+
+  const handleStartButton = (e: object) => {
+    const interval: any = setInterval(() => {
+      if (timeInSeconds > 0) {
+        setTimeInSeconds((previousState: number) => previousState - 1);
+      }
+    }, 1000);
+
+    setIntervalId(interval);
+    setDisableStart(true);
+    setDisableStop(false);
+  };
+
+  const handleStopButton = () => {
+    clearInterval(intervalId);
+    setDisableStop(true);
+    setDisableStart(false);
+  };
+
+  const handleReset = () => {
+    clearInterval(intervalId);
+    setTimeInSeconds(0);
+    setDisableStop(false);
+    setDisableStart(false);
+  };
+
+  function calculateTimeInSeconds(timeInSeconds: number): (number | string)[] {
+    let hours: number = Math.floor(timeInSeconds / 3600);
+    let minutes: number = Math.floor((timeInSeconds - hours * 3600) / 60);
+    let seconds: number = timeInSeconds - hours * 3600 - minutes * 60;
+
+    return [
+      hours < 10 ? `0${hours}` : hours,
+      minutes < 10 ? `0${minutes}` : minutes,
+      seconds < 10 ? `0${seconds}` : seconds,
+    ];
+  }
+
   return (
     <div className="min-w-screen min-h-screen bg-gray-100 flex items-center justify-center px-5 py-5">
       <div className="w-full mx-auto rounded-xl bg-gray-100 shadow-xl text-gray-800 relative overflow-hidden ">
-        <div className="w-full h-40 bg-gradient-to-b from-gray-800 to-gray-700 flex items-end text-right">
+        <div
+          className={`${bgChange} w-full h-40 bg-gradient-to-b from-gray-800 to-gray-700 flex items-end text-right`}
+        >
           <div className="w-full flex justify-center py-5 px-6 text-6xl md:text-8xl text-white font-thin font-mono">
-            <p id="hour">00</p>
+            <p id="hour">{timeArray[0]}</p>
             <span>:</span>
-            <p id="minute">00</p>
+            <p id="minute">{timeArray[1]}</p>
             <span>:</span>
-            <p id="second">00</p>
+            <p id="second">{timeArray[2]}</p>
           </div>
         </div>
         <div className="w-full bg-gradient-to-b from-indigo-400 to-indigo-600">
@@ -35,66 +137,114 @@ const Timer = () => {
 
           <div className="flex w-full">
             <div className="w-1/6 border-r border-b border-indigo-400">
-              <button className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
+              <button
+                disabled={disableStart}
+                onClick={addOneHour}
+                className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
+              >
                 +1
               </button>
             </div>
             <div className="w-1/6 border-r border-b border-indigo-400">
-              <button className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
+              <button
+                disabled={disableStart}
+                onClick={minusOneHour}
+                className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
+              >
                 -1
               </button>
             </div>
             <div className="w-1/6 border-r border-b border-indigo-400">
-              <button className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
+              <button
+                disabled={disableStart}
+                onClick={addOneMinute}
+                className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
+              >
                 +1
               </button>
             </div>
 
             <div className="w-1/6 border-r border-b border-indigo-400">
-              <button className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
+              <button
+                disabled={disableStart}
+                onClick={minusOneMinute}
+                className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
+              >
                 -1
               </button>
             </div>
             <div className="w-1/6 border-r border-b border-indigo-400">
-              <button className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
+              <button
+                disabled={disableStart}
+                onClick={addOneSecond}
+                className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
+              >
                 +1
               </button>
             </div>
             <div className="w-1/6 border-r border-b border-indigo-400">
-              <button className="w-full h-16 outline-none focus:outline-none bg-indigo-700 bg-opacity-10 hover:bg-opacity-20 text-white text-xl font-light">
+              <button
+                disabled={disableStart}
+                onClick={minusOneSecond}
+                className="w-full h-16 outline-none focus:outline-none bg-indigo-700 bg-opacity-10 hover:bg-opacity-20 text-white text-xl font-light"
+              >
                 -1
               </button>
             </div>
           </div>
           <div className="flex w-full">
             <div className="w-1/6 border-r border-b border-indigo-400">
-              <button className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
+              <button
+                disabled={disableStart}
+                onClick={addTenHours}
+                className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
+              >
                 +10
               </button>
             </div>
             <div className="w-1/6 border-r border-b border-indigo-400">
-              <button className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
+              <button
+                disabled={disableStart}
+                onClick={minusTenHours}
+                className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
+              >
                 -10
               </button>
             </div>
             <div className="w-1/6 border-r border-b border-indigo-400">
-              <button className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
+              <button
+                disabled={disableStart}
+                onClick={addTenMinutes}
+                className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
+              >
                 +10
               </button>
             </div>
 
             <div className="w-1/6 border-r border-b border-indigo-400">
-              <button className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
+              <button
+                disabled={disableStart}
+                onClick={minusTenMinutes}
+                className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
+              >
                 -10
               </button>
             </div>
             <div className="w-1/6 border-r border-b border-indigo-400">
-              <button className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light">
+              <button
+                disabled={disableStart}
+                onClick={addTenSeconds}
+                className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
+              >
                 +10
               </button>
             </div>
             <div className="w-1/6 border-r border-b border-indigo-400">
-              <button className="w-full h-16 outline-none focus:outline-none bg-indigo-700 bg-opacity-10 hover:bg-opacity-20 text-white text-xl font-light">
+              <button
+                disabled={disableStart}
+                onClick={minusTenSeconds}
+                className="w-full h-16 outline-none focus:outline-none bg-indigo-700 bg-opacity-10 hover:bg-opacity-20 text-white text-xl font-light"
+              >
                 -10
               </button>
             </div>
@@ -103,9 +253,9 @@ const Timer = () => {
             <div className="w-1/3 border-r border-b border-indigo-400">
               <button
                 type="button"
-                // disabled={disableStart}
+                disabled={disableStart}
                 className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
-                // onClick={handleStartButton}
+                onClick={handleStartButton}
               >
                 START
               </button>
@@ -113,9 +263,9 @@ const Timer = () => {
             <div className="w-1/3 border-r border-b border-indigo-400">
               <button
                 type="button"
-                // disabled={disableStop}
+                disabled={disableStop}
                 className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
-                // onClick={handleStopButton}
+                onClick={handleStopButton}
               >
                 STOP
               </button>
@@ -124,7 +274,7 @@ const Timer = () => {
               <button
                 type="button"
                 className="w-full h-16 outline-none focus:outline-none bg-indigo-700 bg-opacity-10 hover:bg-opacity-20 text-white text-xl font-light"
-                // onClick={handleReset}
+                onClick={handleReset}
               >
                 RESET
               </button>
