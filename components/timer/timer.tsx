66f5b4fde46077b495cb7fc8cf7123a58db0c85f
Ashby McFall <ashbymcfall@hotmail.co.uk>
2022-05-06 10:38:18 +0900

    🐛 stop timer going below 0

--- components/timer/timer.tsx
+++ components/timer/timer.tsx
@@ -101,6 +101,10 @@ const Timer = () => {
     ];
   }
 
+  if (timeInSeconds < 0) {
+    handleReset();
+  }
+
   return (
     <div className="min-w-screen min-h-screen bg-gray-100 flex items-center justify-center px-5 py-5">
       <div className="w-full mx-auto rounded-xl bg-gray-100 shadow-xl text-gray-800 relative overflow-hidden ">
