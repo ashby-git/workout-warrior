d0535aed98917757cf6608a7441f0d39bc3d541e
Ashby McFall <ashbymcfall@hotmail.co.uk>
2022-05-09 16:59:41 +0900

    ✨ add alarm sound when timer ends

--- components/timer/timer.tsx
+++ components/timer/timer.tsx
@@ -6,6 +6,7 @@ const Timer = () => {
   const [intervalId, setIntervalId] = useState<number>(0);
   const [disableStart, setDisableStart] = useState(false);
   const [disableStop, setDisableStop] = useState(false);
+  const [timerEnd, setTimerEnd] = useState(false);
 
   useEffect(() => {
     setTimeArray(calculateTimeInSeconds(timeInSeconds));
@@ -101,192 +102,197 @@ const Timer = () => {
     ];
   }
 
-  if (timeInSeconds < 0) {
+  if (timeInSeconds == 0 && disableStart) {
     handleReset();
+    setTimerEnd(true);
+    setTimeout(() => setTimerEnd(false), 4000);
   }
 
   return (
-    <div className="min-w-screen min-h-screen bg-gray-100 flex items-center justify-center px-5 py-5">
-      <div className="w-full mx-auto rounded-xl bg-gray-100 shadow-xl text-gray-800 relative overflow-hidden ">
-        <div
-          className={`${bgChange} w-full h-40 bg-gradient-to-b from-gray-800 to-gray-700 flex items-end text-right`}
-        >
-          <div className="w-full flex justify-center py-5 px-6 text-6xl md:text-8xl text-white font-thin font-mono">
-            <p id="hour">{timeArray[0]}</p>
-            <span>:</span>
-            <p id="minute">{timeArray[1]}</p>
-            <span>:</span>
-            <p id="second">{timeArray[2]}</p>
+    <>
+      <div className="min-w-screen min-h-screen bg-gray-100 flex items-center justify-center px-5 py-5">
+        <div className="w-full mx-auto rounded-xl bg-gray-100 shadow-xl text-gray-800 relative overflow-hidden ">
+          <div
+            className={`${bgChange} w-full h-40 bg-gradient-to-b from-gray-800 to-gray-700 flex items-end text-right`}
+          >
+            <div className="w-full flex justify-center py-5 px-6 text-6xl md:text-8xl text-white font-thin font-mono">
+              <p id="hour">{timeArray[0]}</p>
+              <span>:</span>
+              <p id="minute">{timeArray[1]}</p>
+              <span>:</span>
+              <p id="second">{timeArray[2]}</p>
+            </div>
           </div>
-        </div>
-        <div className="w-full bg-gradient-to-b from-indigo-400 to-indigo-600">
-          <div className="flex w-full">
-            <div className="w-1/3 border-r border-b border-indigo-400">
-              <div className="flex justify-center items-center w-full h-10 outline-none text-white text-xl font-light">
-                Hours
+          <div className="w-full bg-gradient-to-b from-indigo-400 to-indigo-600">
+            <div className="flex w-full">
+              <div className="w-1/3 border-r border-b border-indigo-400">
+                <div className="flex justify-center items-center w-full h-10 outline-none text-white text-xl font-light">
+                  Hours
+                </div>
+              </div>
+              <div className="w-1/3 border-r border-b border-indigo-400">
+                <div className="flex justify-center items-center w-full h-10 outline-none text-white text-xl font-light">
+                  Minutes
+                </div>
+              </div>
+              <div className="w-1/3 border-r border-b border-indigo-400">
+                <div className="flex justify-center items-center w-full h-10 outline-none text-white text-xl font-light">
+                  Seconds
+                </div>
               </div>
             </div>
-            <div className="w-1/3 border-r border-b border-indigo-400">
-              <div className="flex justify-center items-center w-full h-10 outline-none text-white text-xl font-light">
-                Minutes
+            <div className="flex w-full"></div>
+
+            <div className="flex w-full">
+              <div className="w-1/6 border-r border-b border-indigo-400">
+                <button
+                  disabled={disableStart}
+                  onClick={addOneHour}
+                  className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
+                >
+                  +1
+                </button>
+              </div>
+              <div className="w-1/6 border-r border-b border-indigo-400">
+                <button
+                  disabled={disableStart}
+                  onClick={minusOneHour}
+                  className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
+                >
+                  -1
+                </button>
+              </div>
+              <div className="w-1/6 border-r border-b border-indigo-400">
+                <button
+                  disabled={disableStart}
+                  onClick={addOneMinute}
+                  className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
+                >
+                  +1
+                </button>
+              </div>
+
+              <div className="w-1/6 border-r border-b border-indigo-400">
+                <button
+                  disabled={disableStart}
+                  onClick={minusOneMinute}
+                  className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
+                >
+                  -1
+                </button>
+              </div>
+              <div className="w-1/6 border-r border-b border-indigo-400">
+                <button
+                  disabled={disableStart}
+                  onClick={addOneSecond}
+                  className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
+                >
+                  +1
+                </button>
+              </div>
+              <div className="w-1/6 border-r border-b border-indigo-400">
+                <button
+                  disabled={disableStart}
+                  onClick={minusOneSecond}
+                  className="w-full h-16 outline-none focus:outline-none bg-indigo-700 bg-opacity-10 hover:bg-opacity-20 text-white text-xl font-light"
+                >
+                  -1
+                </button>
               </div>
             </div>
-            <div className="w-1/3 border-r border-b border-indigo-400">
-              <div className="flex justify-center items-center w-full h-10 outline-none text-white text-xl font-light">
-                Seconds
+            <div className="flex w-full">
+              <div className="w-1/6 border-r border-b border-indigo-400">
+                <button
+                  disabled={disableStart}
+                  onClick={addTenHours}
+                  className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
+                >
+                  +10
+                </button>
+              </div>
+              <div className="w-1/6 border-r border-b border-indigo-400">
+                <button
+                  disabled={disableStart}
+                  onClick={minusTenHours}
+                  className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
+                >
+                  -10
+                </button>
+              </div>
+              <div className="w-1/6 border-r border-b border-indigo-400">
+                <button
+                  disabled={disableStart}
+                  onClick={addTenMinutes}
+                  className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
+                >
+                  +10
+                </button>
+              </div>
+
+              <div className="w-1/6 border-r border-b border-indigo-400">
+                <button
+                  disabled={disableStart}
+                  onClick={minusTenMinutes}
+                  className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
+                >
+                  -10
+                </button>
+              </div>
+              <div className="w-1/6 border-r border-b border-indigo-400">
+                <button
+                  disabled={disableStart}
+                  onClick={addTenSeconds}
+                  className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
+                >
+                  +10
+                </button>
+              </div>
+              <div className="w-1/6 border-r border-b border-indigo-400">
+                <button
+                  disabled={disableStart}
+                  onClick={minusTenSeconds}
+                  className="w-full h-16 outline-none focus:outline-none bg-indigo-700 bg-opacity-10 hover:bg-opacity-20 text-white text-xl font-light"
+                >
+                  -10
+                </button>
               </div>
             </div>
-          </div>
-          <div className="flex w-full"></div>
-
-          <div className="flex w-full">
-            <div className="w-1/6 border-r border-b border-indigo-400">
-              <button
-                disabled={disableStart}
-                onClick={addOneHour}
-                className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
-              >
-                +1
-              </button>
-            </div>
-            <div className="w-1/6 border-r border-b border-indigo-400">
-              <button
-                disabled={disableStart}
-                onClick={minusOneHour}
-                className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
-              >
-                -1
-              </button>
-            </div>
-            <div className="w-1/6 border-r border-b border-indigo-400">
-              <button
-                disabled={disableStart}
-                onClick={addOneMinute}
-                className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
-              >
-                +1
-              </button>
-            </div>
-
-            <div className="w-1/6 border-r border-b border-indigo-400">
-              <button
-                disabled={disableStart}
-                onClick={minusOneMinute}
-                className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
-              >
-                -1
-              </button>
-            </div>
-            <div className="w-1/6 border-r border-b border-indigo-400">
-              <button
-                disabled={disableStart}
-                onClick={addOneSecond}
-                className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
-              >
-                +1
-              </button>
-            </div>
-            <div className="w-1/6 border-r border-b border-indigo-400">
-              <button
-                disabled={disableStart}
-                onClick={minusOneSecond}
-                className="w-full h-16 outline-none focus:outline-none bg-indigo-700 bg-opacity-10 hover:bg-opacity-20 text-white text-xl font-light"
-              >
-                -1
-              </button>
-            </div>
-          </div>
-          <div className="flex w-full">
-            <div className="w-1/6 border-r border-b border-indigo-400">
-              <button
-                disabled={disableStart}
-                onClick={addTenHours}
-                className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
-              >
-                +10
-              </button>
-            </div>
-            <div className="w-1/6 border-r border-b border-indigo-400">
-              <button
-                disabled={disableStart}
-                onClick={minusTenHours}
-                className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
-              >
-                -10
-              </button>
-            </div>
-            <div className="w-1/6 border-r border-b border-indigo-400">
-              <button
-                disabled={disableStart}
-                onClick={addTenMinutes}
-                className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
-              >
-                +10
-              </button>
-            </div>
-
-            <div className="w-1/6 border-r border-b border-indigo-400">
-              <button
-                disabled={disableStart}
-                onClick={minusTenMinutes}
-                className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
-              >
-                -10
-              </button>
-            </div>
-            <div className="w-1/6 border-r border-b border-indigo-400">
-              <button
-                disabled={disableStart}
-                onClick={addTenSeconds}
-                className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
-              >
-                +10
-              </button>
-            </div>
-            <div className="w-1/6 border-r border-b border-indigo-400">
-              <button
-                disabled={disableStart}
-                onClick={minusTenSeconds}
-                className="w-full h-16 outline-none focus:outline-none bg-indigo-700 bg-opacity-10 hover:bg-opacity-20 text-white text-xl font-light"
-              >
-                -10
-              </button>
-            </div>
-          </div>
-          <div className="flex w-full">
-            <div className="w-1/3 border-r border-b border-indigo-400">
-              <button
-                type="button"
-                disabled={disableStart}
-                className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
-                onClick={handleStartButton}
-              >
-                START
-              </button>
-            </div>
-            <div className="w-1/3 border-r border-b border-indigo-400">
-              <button
-                type="button"
-                disabled={disableStop}
-                className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
-                onClick={handleStopButton}
-              >
-                STOP
-              </button>
-            </div>
-            <div className="w-1/3 border-r border-b border-indigo-400">
-              <button
-                type="button"
-                className="w-full h-16 outline-none focus:outline-none bg-indigo-700 bg-opacity-10 hover:bg-opacity-20 text-white text-xl font-light"
-                onClick={handleReset}
-              >
-                RESET
-              </button>
+            <div className="flex w-full">
+              <div className="w-1/3 border-r border-b border-indigo-400">
+                <button
+                  type="button"
+                  disabled={disableStart}
+                  className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
+                  onClick={handleStartButton}
+                >
+                  START
+                </button>
+              </div>
+              <div className="w-1/3 border-r border-b border-indigo-400">
+                <button
+                  type="button"
+                  disabled={disableStop}
+                  className="w-full h-16 outline-none focus:outline-none hover:bg-indigo-700 hover:bg-opacity-20 text-white text-xl font-light"
+                  onClick={handleStopButton}
+                >
+                  STOP
+                </button>
+              </div>
+              <div className="w-1/3 border-r border-b border-indigo-400">
+                <button
+                  type="button"
+                  className="w-full h-16 outline-none focus:outline-none bg-indigo-700 bg-opacity-10 hover:bg-opacity-20 text-white text-xl font-light"
+                  onClick={handleReset}
+                >
+                  RESET
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      {timerEnd ? <audio src="/alarm.mp3" autoPlay /> : null}
+    </>
   );
 };
 
